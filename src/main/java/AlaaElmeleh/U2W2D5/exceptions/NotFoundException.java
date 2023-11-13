366deeb033d338069274376dec415f3fb53d6b8e
ElmelehAlaa <alaa.elmeleh@outlook.it>
2023-11-13 14:33:45 +0100

    ripreso progetto venerdi

---
+++ src/main/java/AlaaElmeleh/U2W2D5/exceptions/NotFoundException.java
@@ -0,0 +1,9 @@
+package AlaaElmeleh.U2W2D5.exceptions;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(int id){
+        super("Elemento con id " + id + " non trovato!");
+    }
+    public NotFoundException(NotFoundException e){super(e.getMessage());}
+}
